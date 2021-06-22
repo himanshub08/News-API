e2e795d8e91fb5dde9172af030a66a7b1e2a694d
himansh <himanshubhatnagar2@gmail.com>
2021-06-22 18:42:40 +0530

    news-api

---
+++ website.js
@@ -0,0 +1,48 @@
+console.log("This is my index js file");
+
+// Initialize the news api parameters
+
+let apiKey = '88cea5da6e8dc292eec7b3be6b7ee8f9'
+
+// Grab the news container
+let newsAccordion = document.getElementById('newsAccordion');
+
+// Create an ajax get request
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://gnews.io/api/v4/top-headlines?q=crime&country=in&lannguage=hi&token=${apiKey}`, true);
+
+// What to do when response is ready
+xhr.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+        });
+        newsAccordion.innerHTML = newsHtml;
+    }
+    else {
+        console.log("Some error occured")
+    }
+}
+
+xhr.send()
+
+
